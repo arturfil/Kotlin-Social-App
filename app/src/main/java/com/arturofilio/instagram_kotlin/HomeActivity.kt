5238b712ceea45ff0d5dc1c21c3fc2c4df576201
Arturo Filio Villa <arturfil@hotmail.com>
2019-04-11 15:06:08 -0400

    login page works but there is a horrible line after the login button

--- app/src/main/java/com/arturofilio/instagram_kotlin/HomeActivity.kt
+++ app/src/main/java/com/arturofilio/instagram_kotlin/HomeActivity.kt
@@ -8,7 +8,6 @@ import com.google.firebase.auth.FirebaseAuth
 class HomeActivity : BaseActivity(0) {
 
     private val TAG = "HomeActivity"
-
     private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,20 +15,20 @@ class HomeActivity : BaseActivity(0) {
         setContentView(R.layout.activity_home)
         Log.d(TAG, "onCreate")
 
+        // setting up the menu nav, otherwise the view doesn't know how to handle the menu layout
         setupBottomNavigation()
 
-//        mAuth = FirebaseAuth.getInstance()
-//        mAuth.signOut()
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
 
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//
-//        if(mAuth.currentUser == null) {
-//            startActivity(Intent(this, LoginActivity::class.java))
-//            finish()
-//        }
-//    }
+    override fun onStart() {
+        super.onStart()
+        if(mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
 
 }
