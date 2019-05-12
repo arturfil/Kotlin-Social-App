817b85f0a2732c45fc5782b64fe1a13bc5a48665
Arturo Filio Villa <arturfil@hotmail.com>
2019-05-12 04:05:45 -0400

    created the new recycler view in the home section in order to display the posts

--- app/src/main/java/com/arturofilio/instagram_kotlin/activities/HomeActivity.kt
+++ app/src/main/java/com/arturofilio/instagram_kotlin/activities/HomeActivity.kt
@@ -2,32 +2,33 @@ package com.arturofilio.instagram_kotlin.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import com.arturofilio.instagram_kotlin.R
+import com.arturofilio.instagram_kotlin.utils.FirebaseHelper
+import com.arturofilio.instagram_kotlin.utils.GlideApp
+import com.arturofilio.instagram_kotlin.utils.ValueEventListenerAdapater
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.feed_item.view.*
 
 class HomeActivity : BaseActivity(0) {
-
     private val TAG = "HomeActivity"
+    private lateinit var mFirebase: FirebaseHelper
 
-    private lateinit var mAuth: FirebaseAuth
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState:  Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         Log.d(TAG, "onCreate")
-
         setupBottomNavigation()
 
-        mAuth = FirebaseAuth.getInstance()
-//        mAuth.signOut()
-
-
-        sign_out_text.setOnClickListener {
-            mAuth.signOut();
-        }
-        mAuth.addAuthStateListener {
+        mFirebase = FirebaseHelper(this)
+        mFirebase.auth.addAuthStateListener {
             if (it.currentUser == null) {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
@@ -37,11 +38,53 @@ class HomeActivity : BaseActivity(0) {
 
     override fun onStart() {
         super.onStart()
-
-        if(mAuth.currentUser == null) {
+        val currentUser = mFirebase.auth.currentUser
+        if(currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
+        } else {
+            mFirebase.database.child("feed-post").child(currentUser.uid)
+                .addValueEventListener(ValueEventListenerAdapater{
+                    val posts = it.children.map{ it.getValue(FeedPost::class.java)!!}
+                    Log.d(TAG, "feedPosts: ${posts.joinToString("\n", "\n") }}")
+                    feed_recycler.adapter = FeedAdapter(posts)
+                    feed_recycler.layoutManager = LinearLayoutManager(this)
+            })
         }
     }
 
 }
+
+class FeedAdapter(private val posts: List<FeedPost>): RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
+    class  ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout
+            .feed_item,parent,false)
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, positition: Int) {
+        holder.view.post_image.loadImage(posts[positition].image)
+    }
+
+    private fun ImageView.loadImage(image: String) {
+        GlideApp.with(this).load(image).centerCrop().into(this)
+    }
+
+    override fun getItemCount() = posts.size
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/arturofilio/instagram_kotlin/activities/ShareActivity.kt
+++ app/src/main/java/com/arturofilio/instagram_kotlin/activities/ShareActivity.kt
@@ -5,15 +5,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.arturofilio.instagram_kotlin.R
+import com.arturofilio.instagram_kotlin.models.User
 import com.arturofilio.instagram_kotlin.utils.CameraHelper
 import com.arturofilio.instagram_kotlin.utils.FirebaseHelper
 import com.arturofilio.instagram_kotlin.utils.GlideApp
+import com.arturofilio.instagram_kotlin.utils.ValueEventListenerAdapater
+import com.google.firebase.database.ServerValue
 import kotlinx.android.synthetic.main.activity_share.*
+import java.sql.Timestamp
+import java.util.*
 
 class ShareActivity : BaseActivity(2) {
     private val TAG = "ShareActiviy"
     private lateinit var mCamera: CameraHelper
     private lateinit var mFirebase: FirebaseHelper
+    private lateinit var mUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,10 @@ class ShareActivity : BaseActivity(2) {
 
         back_image.setOnClickListener{finish()}
         share_text.setOnClickListener{share()}
+
+        mFirebase.currentUserReference().addValueEventListener(ValueEventListenerAdapater{
+            mUser = it.getValue(User::class.java)!!
+        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -43,10 +53,21 @@ class ShareActivity : BaseActivity(2) {
             mFirebase.storage.child("users").child(mFirebase.auth.currentUser!!.uid).child("images")
                 .child(imageUri.lastPathSegment).putFile(imageUri).addOnCompleteListener{
                     if (it.isSuccessful) {
+                        val imageDownloadUrl = it.result.downloadUrl!!.toString()
                         mFirebase.database.child("images").child(uid).push()
-                            .setValue(it.result.downloadUrl!!.toString())
+                            .setValue(imageDownloadUrl)
                             .addOnCompleteListener{
                                 if(it.isSuccessful) {
+                                    mFirebase.database.child("feed-post").child(uid)
+                                        .push()
+                                        .setValue(mkFeedPost(uid, imageDownloadUrl))
+                                        .addOnCompleteListener{
+                                            if(it.isSuccessful) {
+                                                startActivity(Intent(this,
+                                                    ProfileActivity::class.java))
+                                                finish()
+                                            }
+                                        }
                                     startActivity(Intent(this,
                                         ProfileActivity::class.java))
                                     finish()
@@ -60,4 +81,26 @@ class ShareActivity : BaseActivity(2) {
                 }
         }
     }
+
+    private fun mkFeedPost(uid: String, imageDownloadUrl: String): FeedPost {
+        return FeedPost(
+            uid = uid,
+            username = mUser.username,
+            image = imageDownloadUrl,
+            caption = caption_input.text.toString(),
+            photo = mUser.photo
+        )
+    }
+
 }
+
+data class FeedPost(val uid: String = "", val username: String = "",
+                    val image: String = "", val likesCount: Int = 0,
+                    val commentsCount: Int = 0, val caption: String = "",
+                    val comments: List<Comment> = emptyList(),
+                    val timestamp: Any = ServerValue.TIMESTAMP, val photo: String? = null) {
+    fun timestampDate(): Date = Date(timestamp as Long)
+}
+
+
+data class Comment(val uid: String, val username: String, val text: String)
